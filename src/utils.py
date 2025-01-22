bf3c2a084f6ae1b5259c2c428ffc019138e7d5d2
Manoj Kumar <mkmkumar092@gmail.com>
2025-01-22 16:28:30 +0530

    hyperparameter training

--- src/utils.py
+++ src/utils.py
@@ -8,6 +8,7 @@ import dill
 import pickle
 # from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 
 def save_object(file_path,obj):
@@ -23,15 +24,22 @@ def save_object(file_path,obj):
         raise CustomException(e,sys)
     
 
-def evaluate_model(X_train,y_train,X_test,y_test,models):
+def evaluate_model(X_train,y_train,X_test,y_test,models,param):
     try:
         
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para = param[list(models.keys())[i]]
 
+            gs  = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
+
+            # model.fit(X_train,y_train)
 
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
@@ -41,7 +49,6 @@ def evaluate_model(X_train,y_train,X_test,y_test,models):
 
             report[list(models.keys())[i]]=test_model_score
 
-            
 
             return report
         
